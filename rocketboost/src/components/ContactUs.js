b6d83da1c22bfaf4051c6d8ca58a19e43120fc9e
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-05-15 00:38:09 +0200

    contactus created

---
+++ rocketboost/src/components/ContactUs.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function ContactUs() {
+  return (
+    <div className="contact-us-section">
+      <h2>Contact Us</h2>
+    </div>
+  );
+}
+export default ContactUs;
