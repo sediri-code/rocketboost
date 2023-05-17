fa9e76d75d35231fb00745941bd5984bc7b38bf6
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-05-17 23:52:50 +0200

    tried new test

---
+++ rocketboost/src/components/ServerDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServerDetails = () => {
+  return (
+    <div>
+      <h2>Server Details</h2>
+      {/* Add server details content here */}
+    </div>
+  );
+};
+
+export default ServerDetails;
